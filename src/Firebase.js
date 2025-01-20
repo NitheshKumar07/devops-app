719c251ffbfc66519e1022827f4a1f4a1819f04e
NitheshKumar07 <niteshjaanu08072000@gmail.com>
2025-01-20 13:27:08 +0530

    api add

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,14 +4,13 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7pTt126DUSITIRyG4xNnVmDH50wWAi7w",
-  authDomain: "taskbuddy-67332.firebaseapp.com",
+  authDomain: "taskbuddy-khaki.vercel.app",
   projectId: "taskbuddy-67332",
   storageBucket: "taskbuddy-67332.firebasestorage.app",
   messagingSenderId: "914929371404",
   appId: "1:914929371404:web:7709e606b2e8aceaafe129",
   measurementId: "G-E3N2LRTB4V",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
